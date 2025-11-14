13e593311fd5ea0e6c291085a76efd5a3f15f1f4
DizzyServer <mkulvanovskis@gmail.com>
2025-11-13 20:47:46 -0500

    Screen.hpp ready

---
+++ Screen.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#ifndef SCREEN_HPP
+#define SCREEN_HPP
+
+#include <vector>
+
+class Screen
+{
+    private:
+        std::vector<std::vector<char>> data;
+        int rows;
+        int columns;
+    public:
+        Screen(int x, int y, char fill = ' ')
+            : data(y, std::vector<char>(x, fill))
+              , rows(x)
+              , columns(y) {}
+    
+    char& at (int x, int y);
+    const char& at  (int x, int y) const;
+    // operators
+    char& operator()(int x, int y);
+    const char& operator()(int x, int y);
+
+    int rowCount() const;
+    int columnCount() const;
+}   
+
+
+
+#endif
